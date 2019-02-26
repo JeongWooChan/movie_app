13e89591011eba9a4ca85aeaf03eddd067afd359
root <root@DESKTOP-HNE37SC.localdomain>
2019-02-26 19:58:58 +0900

    Updates

---
+++ precache-manifest.16dd2dc1ddd49025310e90abccccc36c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "1acde5d64a8de914cf9f",
+    "url": "/movie_app/static/js/main.1acde5d6.chunk.js"
+  },
+  {
+    "revision": "5178fb6fe39fb9e436b0",
+    "url": "/movie_app/static/js/2.5178fb6f.chunk.js"
+  },
+  {
+    "revision": "1acde5d64a8de914cf9f",
+    "url": "/movie_app/static/css/main.d105d438.chunk.css"
+  },
+  {
+    "revision": "7852faf0c618a330765cf65ee915c3e8",
+    "url": "/movie_app/index.html"
+  }
+];
